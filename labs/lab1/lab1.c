33c6d508a27dd94155d059b0529f7426ea216a22
Shaun Wallace <shaunw.wallace@uri.edu>
2026-01-21 17:01:12 -0500

    add lab0 and lab1 stencil

---
+++ labs/lab1/lab1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+    this is the main function. It takes in the command line arguments.
+    you will want to ensure you create a function for each piece of logic in your code.
+    Why? Because it makes your code easier to read and understand. It also makes it easier to test. ;)
+*/
+int main(int argc, char *argv[]) {
+
+}
