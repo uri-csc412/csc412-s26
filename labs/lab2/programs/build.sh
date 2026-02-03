fbc64e1961c4c8c93188550097ef404852bc8503
Shaun Wallace <shaunw.wallace@uri.edu>
2026-02-03 11:37:38 -0500

    lab2

---
+++ labs/lab2/programs/build.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/env bash
+
+### compile all c code in the current directory
+
+### Hints:
+### how do you get the current directory in bash?
+### how do you store the output of variables in bash?
+
+# this will currently only compile linebreaker
+gcc -Wall -Werror -o linebreaker linebreaker.c
---
+++ labs/lab2/programs/lab.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/env bash
+
+### call the build script to compile all c code
+
+
+### output to a text file using both programs
+
+
+### output to terminal using both programs
+
+
